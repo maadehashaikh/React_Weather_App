0cfea333e2725418b801e38bf3668e20b076dbea
maadehashaikh <maadeham@gmail.com>
2024-10-12 23:51:38 +0500

    done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 import WeatherData from "./component/WeatherData";
-import { ToastContainer, toast } from "react-toastify";
+import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 function App() {
